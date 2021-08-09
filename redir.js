08c872275ea02bab9aa486cb11fac3436680b8b8
naafstore <86194924+naafstore@users.noreply.github.com>
2021-08-09 21:20:52 +0700

    Create redir.js

---
+++ redir.js
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Order Now</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+	<center><img src="https://1.bp.blogspot.com/-RTK0ll2Vx0g/YJKC00ZeOPI/AAAAAAAABLk/VFG-YBkJOYIJTdPpS4SOEZV0XF2Q8HmcACLcBGAsYHQ/s0/loading.gif"></center>
+<script>
+var jsonContent = {"akun" : [
+  {
+    "aff": "http://s.click.aliexpress.com/deep_link.htm?aff_short_key=pYTSa1BE&dl_target_url=https://www.aliexpress.com/item/",
+  }
+]}
+
+ var random = jsonContent.akun[Math.floor(Math.random() * jsonContent.akun.length)];
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+var first = getUrlVars()["item"];
+if(first>0){
+window.location.href = random.aff+first+'.html';} else {window.location.href = '//';}
+</script>
+</body>
+</html>
